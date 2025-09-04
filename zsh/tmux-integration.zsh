97dd8d9940b8ee73ae5c4358cee05c30bdee5563
Siddharth Mohanty <neosiddharth@gmail.com>
2025-09-03 21:46:02 -0700

    formatting

--- zsh/tmux-integration.zsh
+++ zsh/tmux-integration.zsh
@@ -1,17 +1,13 @@
 # place this file in ~/.zsh/tmux-integration.zsh
 
 if [[ -n "$TMUX" ]]; then
-  export TMUX_PROJ_PATH="$(tmux display-message -p -F "#{pane_current_path}")"
-  alias c='cd $TMUX_PROJ_PATH'
-fi
-
-if [[ -n "$TMUX" ]]; then
+    export TMUX_PROJ_PATH="$(tmux display-message -p -F "#{pane_current_path}")"
+    alias c='cd $TMUX_PROJ_PATH'
     alias tmat='tmux switch-client -t'
 else
     alias tmat='tmux attach -t'
 fi
 alias tml='tmux list-sessions'
-
 
 tn() {
     tmux new -A -s "$(basename "$(pwd)")" -c "$(pwd)"
@@ -20,6 +16,6 @@ tn() {
 # tmux auto attach
 if [ -z "$TMUX" ] && [ -z "$SSH_CONNECTION" ]; then
     if tmux has-session 2>/dev/null; then
-      tmux attach || zsh # ensure that a zsh is left behind after detach
+        tmux attach || zsh # ensure that a zsh is left behind after detach
     fi
 fi
