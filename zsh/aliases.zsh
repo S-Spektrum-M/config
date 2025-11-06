fc11ca1bff0d34632aa4c88e18ee6afeaf2c275f
Siddharth Mohanty <neosiddharth@gmail.com>
2025-11-05 16:02:22 -0800

    compiler aliases update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,8 +11,8 @@ alias rm='safe-rm'
 
 # C/C++ compiler
 alias g++='/usr/bin/g++-14 --std=c++23'
-alias clang++='/usr/bin/clang++-20 --std=c++23'
-alias clang='/usr/bin/clang-20 --std=c++23'
+alias clang++='/usr/bin/clang++-21 --std=c++23'
+alias clang='/usr/bin/clang-21 --std=c++23'
 
 # Quick Calculator
 alias qc='nvim "/tmp/NVIMcalc_buf.py" && python3 "/tmp/NVIMcalc_buf.py"; rm /tmp/NVIMcalc_buf.py'
