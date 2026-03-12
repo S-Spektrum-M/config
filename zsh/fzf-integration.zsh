072589fbb2608ed26f85615ce1f0c7005fe4e062
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-11 19:18:15 -0700

    fix: ignore hidden dirs in alt-c

--- zsh/fzf-integration.zsh
+++ zsh/fzf-integration.zsh
@@ -1,4 +1,5 @@
 export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#080a0c,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+export FZF_ALT_C_OPTS="--walker=dir,follow"
 
 # Source FZF keybindings and completions if they exist
 [ -f "$HOME/.fzf.zsh" ] && source "$HOME/.fzf.zsh"
