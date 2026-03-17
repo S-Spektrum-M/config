d002c1632a35ffc910580759947c1851467d1dc1
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-17 01:41:31 -0700

    source custom completion

--- zsh/opts.zsh
+++ zsh/opts.zsh
@@ -4,3 +4,5 @@ setopt SHARE_HISTORY # Share history between all sessions
 bindkey -v
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' menu select
+autoload -U compinit
+compinit -i
