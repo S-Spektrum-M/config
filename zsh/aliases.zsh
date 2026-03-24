27a5c29766c9966d558e5308b2785d1b688940a4
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-23 23:11:24 -0700

    feature: alert command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,3 +14,13 @@ alias clang='/usr/bin/clang-21 --std=c++23'
 
 # Quick Calculator
 alias qc='nvim "/tmp/NVIMcalc_buf.py" && python3 "/tmp/NVIMcalc_buf.py"; rm /tmp/NVIMcalc_buf.py'
+
+alert() {
+  "$@"
+  local exit_code=$?
+  if [ $exit_code = 0 ]; then
+    notify-send --urgency=normal -i terminal "Done" "$*"
+  else
+    notify-send --urgency=critical -i error "Failed (exit $exit_code)" "$*"
+  fi
+}
