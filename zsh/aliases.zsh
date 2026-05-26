52d5f69e0679bb81ffe43acfbe90cf0850157700
Siddharth Mohanty <neosiddharth@gmail.com>
2026-05-26 05:34:36 -0700

    feat(zsh): open editor in window 0 when not running & bypass switching with args in nv

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,21 +14,37 @@ nv() {
 
     if [[ -n "$TMUX" && "$new_instance" = false && ${#args[@]} -eq 0 ]]; then
         local editor_cmd="${EDITOR:-nvim}"
-        editor_cmd="${editor_cmd%% *}"
-        local editor_name="${editor_cmd##*/}"
+        local editor_cmd_only="${editor_cmd%% *}"
+        local editor_name="${editor_cmd_only##*/}"
 
         local target_window=""
         local win_id win_active pane_cmd
+        local editor_is_running=false
         while read -r win_id win_active pane_cmd; do
-            if [[ "$win_active" == "0" && "$pane_cmd" == "$editor_name" ]]; then
-                target_window="$win_id"
-                break
+            if [[ "$pane_cmd" == "$editor_name" ]]; then
+                editor_is_running=true
+                if [[ "$win_active" == "0" ]]; then
+                    target_window="$win_id"
+                fi
             fi
         done < <(tmux list-panes -s -F '#{window_id} #{window_active} #{pane_current_command}' 2>/dev/null)
 
         if [[ -n "$target_window" ]]; then
             tmux select-window -t "$target_window"
             return 0
+        fi
+
+        if [[ "$editor_is_running" == "false" ]]; then
+            local cur_win
+            cur_win=$(tmux display-message -p '#{window_index}' 2>/dev/null)
+            if [[ "$cur_win" != "0" ]]; then
+                if tmux select-window -t :0 2>/dev/null; then
+                    tmux send-keys "${editor_cmd}" C-m
+                else
+                    tmux new-window -t :0 -c "$PWD" "${editor_cmd}"
+                fi
+                return 0
+            fi
         fi
     fi
 
