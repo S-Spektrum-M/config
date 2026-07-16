6a6721ae8d40b88e33aaa14114af594fbeab2bf3
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-16 01:36:03 -0700

    feat: codex theme
    feat: supress global compinit

---
+++ zsh/.zshenv
@@ -0,0 +1,3 @@
+# Ubuntu initializes completion from /etc/zsh/zshrc unless this is set.
+# The user configuration initializes it after adding custom completions to fpath.
+skip_global_compinit=1
--- zsh/opts.zsh
+++ zsh/opts.zsh
@@ -4,5 +4,6 @@ setopt SHARE_HISTORY # Share history between all sessions
 bindkey -v
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' menu select
+fpath=(~/.zsh/completions $fpath)
 autoload -U compinit
 compinit -i
