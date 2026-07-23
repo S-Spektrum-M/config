3502210544f5aec07f0bc665357beaea316e6635
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-23 05:23:05 -0700

    perf: use compiled cominit

--- zsh/opts.zsh
+++ zsh/opts.zsh
@@ -5,5 +5,22 @@ bindkey -v
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' menu select
 fpath=(~/.zsh/completions $fpath)
-autoload -U compinit
-compinit -i
+autoload -Uz compinit
+
+_zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
+_zcompcheck="${_zcompdump}.last-check"
+_zcompcheck_expired=(${~_zcompcheck}(Nmh+24))
+
+# Audit completion directories once per day. Between audits, skip the security
+# scan and load the compiled completion dump.
+if [[ ! -s "$_zcompdump" ||
+      ! -e "$_zcompcheck" ||
+      ${#_zcompcheck_expired} -ne 0 ]]; then
+    compinit -i -d "$_zcompdump"
+    : >| "$_zcompcheck"
+    zcompile -R "$_zcompdump"
+else
+    compinit -C -d "$_zcompdump"
+fi
+
+unset _zcompdump _zcompcheck _zcompcheck_expired
