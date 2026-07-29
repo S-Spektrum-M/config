ebe4a421ebb6f508e34b34f372e11db8e39b2697
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-29 13:48:25 -0700

    fix(zsh): cleaned up banner

--- zsh/banner.zsh
+++ zsh/banner.zsh
@@ -1,19 +1,25 @@
 # Print the Mach banner when an interactive Zsh session starts.
 
+# (( SHLVL == 1 )) || return 0                  # Only print the banner for the first shell level
+
 function mach_banner() {
+    # local -a banner=(
+    #     '                        █▀▄▀█ ▄▀█ █▀▀ █░█                        '
+    #     '                 .      █░▀░█ █▀█ █▄▄ █▀█      .                 '
+    #     '                //                             \\                '
+    #     '               //                               \\               '
+    #     '              //             zsh 0.9             \\              '
+    #     '             //                _._                \\             '
+    #     '          .---.              .//|\\.              .---.          '
+    #     '________ / .-. \_________..-~ _.-._ ~-..________ / .-. \_________'
+    #     "         \ ._. /    H-     '--.___.--'     -H    \ ._. /         "
+    #     '          •---•     H          [H]          H     •---•          '
+    #     '                   _H_         _H_         _H_                   '
+    #     '                   UUU         UUU         UUU                   '
+    # )
     local -a banner=(
         '                        █▀▄▀█ ▄▀█ █▀▀ █░█                        '
-        '                 .      █░▀░█ █▀█ █▄▄ █▀█      .                 '
-        '                //                             \\                '
-        '               //                               \\               '
-        '              //             zsh 0.9             \\              '
-        '             //                _._                \\             '
-        '          .---.              .//|\\.              .---.          '
-        '________ / .-. \_________..-~ _.-._ ~-..________ / .-. \_________'
-        "         \ ._. /    H-     '--.___.--'     -H    \ ._. /         "
-        '          •---•     H          [H]          H     •---•          '
-        '                   _H_         _H_         _H_                   '
-        '                   UUU         UUU         UUU                   '
+        '                        █░▀░█ █▀█ █▄▄ █▀█                        '
     )
     local banner_width=65
     local padding=0
@@ -21,11 +27,13 @@ function mach_banner() {
 
     (( COLUMNS > banner_width )) && padding=$(( (COLUMNS - banner_width) / 2 ))
 
+    print -- $'\n'                              # Print a padding newline before the banner
     print -n -- $'\e[36m'
     for line in "${banner[@]}"; do
-        printf '%*s%s\n' "$padding" '' "$line"
+        printf '%*s%s\n' "$padding" '' "$line"  # centered
+        # print -r -- "$line"                   # left-aligned
     done
-    print -n -- $'\e[0m'
+    print -- $'\e[0m'                           # Print a padding newline after the banner
 }
 
 mach_banner
